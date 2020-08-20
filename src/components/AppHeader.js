28fbf578346b774306551e98c5e34927ce562fdf
Sathyajith-vunet <sathyajith@vunetsystems.com>
2020-08-20 19:15:33 +0530

    2nd Day

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 function AppHeader() {
         return (
-            <div className="header">
+            <nav className="navbar navbar-expand-lg">
                 <h1>Movie Details App</h1>
-            </div>
+                {/* <div className="collapse navbar-collapse" id="navbarSupportedContent"> */}
+                    <form className="form-inline my-2 my-lg-0">
+                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                {/* </div> */}
+            </nav>
         )
 }
 
