455a4b16917c5f71cf9e8a648a307317dec7d877
Sathyajith-vunet <sathyajith@vunetsystems.com>
2020-09-03 19:34:28 +0530

    Updates

---
+++ precache-manifest.99a52be4f77e078dae6cb8be293f8e25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669d2028170223e64c0c17eae416a2a9",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "4937d492c0d1bfa04414",
+    "url": "/movies-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c53aac9f15aa9ee13b18",
+    "url": "/movies-app/static/css/main.d3c1e602.chunk.css"
+  },
+  {
+    "revision": "4937d492c0d1bfa04414",
+    "url": "/movies-app/static/js/2.fba53597.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movies-app/static/js/2.fba53597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c53aac9f15aa9ee13b18",
+    "url": "/movies-app/static/js/main.f8162414.chunk.js"
+  },
+  {
+    "revision": "6f4ce7a2b201b6f9f6f8",
+    "url": "/movies-app/static/js/runtime-main.94bcfe81.js"
+  }
+]);
